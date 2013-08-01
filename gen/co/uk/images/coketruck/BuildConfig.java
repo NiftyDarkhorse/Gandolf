6591bcec62b92d533953afe11122e0eb5383051e
Francisco Dans <fdansv@gmail.com>
2013-08-01 14:54:46 +0100

    First commit
    First commit indeed!

---
+++ gen/co/uk/images/coketruck/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package co.uk.images.coketruck;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/co/uk/images/coketruck/MainActivity.java
@@ -0,0 +1,76 @@
+package co.uk.images.coketruck;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Movie;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends Activity {
+
+    Movie movie;
+    Canvas animationCanvasOne;
+    View thisView;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView view = new GifWebView(this);
+        setContentView(view);
+
+//        main = new Canvas();
+//
+//
+//        thisView = findViewById(R.id.mainLayout);
+//
+//        thisView.draw(animationCanvasOne);
+//        new GetGifFromNetwork().execute();
+
+
+    }
+
+    class GetGifFromNetwork extends AsyncTask<Void, Integer, Movie> {
+        protected Movie doInBackground(Void... objects) {
+            InputStream in;
+            try {
+                in = new URL("http://10.254.26.28:8888/front.gif").openStream();
+                movie = Movie.decodeStream(in);
+                return movie;
+            }
+            catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+
+            }
+            return null;
+
+              //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        protected void onPostExecute(Movie io) {
+            super.onPostExecute(io);    //To change body of overridden methods use File | Settings | File Templates.
+
+
+            movie.draw(animationCanvasOne, 200,200);
+            thisView.invalidate();
+            System.out.println("Gif loaded");
+        }
+    }
+    public class GifWebView extends WebView {
+
+        public GifWebView(Context context) {
+            super(context);
+            loadUrl("http://10.254.26.28:8888/front.gif");
+        }
+    }
+
+}
