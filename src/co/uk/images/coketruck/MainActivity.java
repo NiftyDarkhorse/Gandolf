3d4c48d56bc7fa9659a46e9988386f64d046a4e4
Francisco Dans <fdansv@gmail.com>
2013-09-19 12:51:17 +0100

    Reinforced the app so that it doesn't crashes, also locked on landscape

--- src/co/uk/images/coketruck/MainActivity.java
+++ src/co/uk/images/coketruck/MainActivity.java
@@ -1,45 +1,93 @@
 package co.uk.images.coketruck;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
-
 import android.widget.TextView;
+
 import com.pusher.client.Pusher;
 import com.pusher.client.channel.Channel;
 import com.pusher.client.channel.SubscriptionEventListener;
 import com.pusher.client.connection.ConnectionState;
 import com.pusher.client.connection.ConnectionStateChange;
 import com.pusher.client.connection.ConnectionEventListener;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends Activity {
     Pusher pusher;
     RelativeLayout ll;
     Typeface rt;
     public boolean newGif = false;
+    String sessionID;
+    boolean gotImage = false;
+    private boolean subscribed;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         rt = Typeface.createFromAsset(this.getAssets(),"Roboto-Thin.ttf");
         setContentView(R.layout.main);
-        TextView fb = (TextView) findViewById(R.id.facebookText);
-        fb.setTypeface(rt);
-        TextView vk = (TextView) findViewById(R.id.vkText);
-        vk.setTypeface(rt);
+        TextView email = (TextView) findViewById(R.id.emailText);
+        email.setTypeface(rt);
+        ImageButton emailButton = (ImageButton) findViewById(R.id.emailButton);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(gotImage){
+                    EditText emailField = (EditText) findViewById(R.id.emailEdit);
+                    String emailText = emailField.getText().toString();
+                    String emailReqURL = "http://ccht.imagesstage.co.uk/email.php?session="+sessionID+"&email="+emailText;
+                    new GetURL().execute(emailReqURL);
+                }
+            }
+        });
         ll = (RelativeLayout) this.findViewById(R.id.mainLayout);
-        if (pusher==null){
+        if (!subscribed){
             this.enablePusher();
         }
     }
+    protected class GetURL extends AsyncTask<String,Void,Void>{
 
+        @Override
+        protected Void doInBackground(String... url) {
+            String response = "";
+            try {
+                HttpClient client = new DefaultHttpClient();
+                String getURL = url[0];
+                System.out.println(getURL);
+                HttpGet get = new HttpGet(getURL);
+                HttpResponse responseGet = null;
+                responseGet = client.execute(get);
+                HttpEntity resEntityGet = responseGet.getEntity();
+                if (resEntityGet != null) {
+                    response = EntityUtils.toString(resEntityGet);
+                }
+                System.out.println("sent to "+response);
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+
+            return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+    }
     protected void enablePusher(){
         pusher = new Pusher("c0a25a463fbbf294a4a8");
         pusher.connect(new ConnectionEventListener() {
@@ -54,25 +102,28 @@ public class MainActivity extends Activity {
         }, ConnectionState.ALL);
 
         Channel channel = pusher.subscribe( "gifs" );
+        subscribed = true;
         channel.bind("giffinish", new SubscriptionEventListener() {
 
             @Override
-            public void onEvent(String channel, String event, String data) {
+            public void onEvent(String channel, String event, final String data) {
                 MainActivity.this.runOnUiThread(new Runnable() {
                     public void run() {
                         newGif = true;
-                        System.out.println("Invalidating view");
+                        String gifURL=data.replace("\"","");
+                        sessionID = gifURL;
                         WebView view1 = new WebView(MainActivity.this);
-                        view1.loadUrl("http://10.254.26.69/front.gif");
+                        view1.loadUrl("http://cocacolahappiness.s3-website-eu-west-1.amazonaws.com/"+gifURL+".gif");
                         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(420,620);
                         params.leftMargin = 30;
                         params.topMargin = 40;
                         ll.addView(view1, params);
                         ll.invalidate();
-                        System.out.println("Invalidated");
+                        gotImage = true;
                     }
                 });
             }
         });
     }
 }
+
