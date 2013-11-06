fe516c01b8c2f9084a20a1d0db6c5596b5a967fd
Francisco Dans <fdansv@gmail.com>
2013-11-06 11:45:09 +0000

    added new styles

--- src/co/uk/images/coketruck/MainActivity.java
+++ src/co/uk/images/coketruck/MainActivity.java
@@ -1,14 +1,11 @@
 package co.uk.images.coketruck;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.Display;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.*;
 
@@ -26,7 +23,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 
 public class MainActivity extends Activity {
     Pusher pusher;
@@ -36,43 +32,78 @@ public class MainActivity extends Activity {
     String sessionID;
     boolean gotImage = false;
     private boolean subscribed;
+    private boolean gotForm;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        rt = Typeface.createFromAsset(this.getAssets(),"Montserrat-Regular.ttf");
-        setContentView(R.layout.main);
-        TextView email = (TextView) findViewById(R.id.emailText);
-        ((TextView) findViewById(R.id.textView)).setTypeface(rt);
-
-        email.setTypeface(rt);
-        Button emailButton = (Button) findViewById(R.id.emailButton);
-        emailButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if(gotImage){
-                    EditText emailField = (EditText) findViewById(R.id.emailEdit);
-                    EditText nameField = (EditText) findViewById(R.id.nameEdit);
-                    String emailText = emailField.getText().toString();
-                    String nameText = nameField.getText().toString();
-                    if(!emailText.contains("@")){
-                        Toast.makeText(MainActivity.this, "Please use a valid email address", Toast.LENGTH_SHORT).show();
-                    }
-                    else{
-                        String emailReqURL = "http://ccht.imagesstage.co.uk/email.php?session="+sessionID+"&email="+emailText+"&name="+nameText;
-                        Toast.makeText(MainActivity.this, "Sending...", Toast.LENGTH_SHORT).show();
-                        new GetURL().execute(emailReqURL);
-                        emailField.setText("");
-                        nameField.setText("");
-                    }
-                }
-            }
-        });
-        ll = (RelativeLayout) this.findViewById(R.id.mainLayout);
+        rt = Typeface.createFromAsset(this.getAssets(), "Montserrat-Regular.ttf");
+        setContentView(R.layout.setone);
+        setButtonListenerOne();
         if (!subscribed){
             this.enablePusher();
         }
     }
+
+    private void setButtonListenerOne() {
+        ImageButton next1 = (ImageButton) this.findViewById(R.id.next);
+        next1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("next pressed");
+                if(gotImage){
+                    MainActivity.this.setContentView(R.layout.formview);
+                    gotForm = true;
+                    setButtonListenerTwo();
+                }
+            }
+        });
+    }
+    private void setButtonListenerTwo() {
+        ImageButton next2 = (ImageButton) this.findViewById(R.id.next2);
+        next2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("next pressed");
+                if(gotForm){
+                    MainActivity.this.initEmailSending();
+
+
+                }
+            }
+        });
+    }
+    private void setButtonListenerThree() {
+        ImageButton next2 = (ImageButton) this.findViewById(R.id.next2);
+        next2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                gotForm = false;
+                gotImage = false;
+                MainActivity.this.setContentView(R.layout.setone);
+
+            }
+        });
+    }
+
+    private WebView generateWebView(String url, boolean scrolled) {
+        WebView view = new WebView(this);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(toDp(320), toDp(480));
+        params.setMargins(toDp(5), scrolled? toDp(5): -toDp(235), 0, 0);
+        view.setLayoutParams(params);
+        view.loadUrl(url);
+
+        return view;
+//        view.setWebViewClient(new WebViewClient() {
+//            public void onPageFinished(WebView view, String url) {
+//                view.scrollTo(0, 240);
+//            }
+//        });
+    }
+    private int toDp(int measure){
+        final float scale = this.getResources().getDisplayMetrics().density;
+        int pixels = (int) (measure * scale + 0.5f);
+        return pixels;
+    }
+
     protected class GetURL extends AsyncTask<String,Void,Void>{
 
         @Override
@@ -89,8 +120,8 @@ public class MainActivity extends Activity {
                 if (resEntityGet != null) {
                     response = EntityUtils.toString(resEntityGet);
                 }
-                System.out.println("sent to "+response);
             } catch (IOException e) {
+
                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
             }
 
@@ -102,6 +133,7 @@ public class MainActivity extends Activity {
             super.onPostExecute(aVoid);
             Toast.makeText(MainActivity.this, "Thanks! Your happiness moment has been submitted", Toast.LENGTH_SHORT).show();
         }
+
     }
     protected void enablePusher(){
         pusher = new Pusher("c0a25a463fbbf294a4a8");
@@ -116,7 +148,7 @@ public class MainActivity extends Activity {
 
         }, ConnectionState.ALL);
 
-        Channel channel = pusher.subscribe( "gifs" );
+        Channel channel = pusher.subscribe("gifs");
         subscribed = true;
         channel.bind("giffinish", new SubscriptionEventListener() {
 
@@ -127,23 +159,38 @@ public class MainActivity extends Activity {
                         newGif = true;
                         String gifURL=data.replace("\"","");
                         sessionID = gifURL;
-                        WebView view1 = new WebView(MainActivity.this);
-                        ImageView gifMask = new ImageView(getBaseContext());
-                        gifMask.setImageDrawable(getResources().getDrawable(R.drawable.gifmask));
-
-                        view1.loadUrl("http://cocacolahappiness.s3-website-eu-west-1.amazonaws.com/"+gifURL+".gif");
-                        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(420,620);
-                        params.leftMargin = 30;
-                        params.topMargin = 40;
-
-                        ll.addView(view1, params);
-                        ll.addView(gifMask, params);
-                        ll.invalidate();
+                        RelativeLayout frame1= (RelativeLayout) findViewById(R.id.frame1);
+                        RelativeLayout frame2= (RelativeLayout) findViewById(R.id.frame2);
+                        String url = "http://cocacolahappiness.s3-website-eu-west-1.amazonaws.com/"+gifURL+".gif";
+                        WebView wb = generateWebView(url, true);
+                        frame1.addView(wb, 0);
+                        frame1.setRotation(-15);
+                        WebView second = generateWebView(url, false);
+                        frame2.addView(second, 0);
+                        frame2.setRotation(15);
                         gotImage = true;
                     }
                 });
             }
         });
     }
+    private void initEmailSending() {
+        EditText emailField = (EditText) findViewById(R.id.emailField);
+        EditText nameField = (EditText) findViewById(R.id.nameField);
+        EditText captionField = (EditText) findViewById(R.id.captionField);
+        String emailText = emailField.getText().toString();
+        String nameText = nameField.getText().toString();
+        String captionText = captionField.getText().toString();
+        if(!emailText.contains("@")){
+            Toast.makeText(MainActivity.this, "Please use a valid email address", Toast.LENGTH_SHORT).show();
+        }
+        else if(nameText.equals("")){
+            Toast.makeText(MainActivity.this, "Please fill the name field", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            String emailReqURL = "http://ccht.imagesstage.co.uk/email.php?session="+sessionID+"&email="+emailText+"&name="+nameText+"&caption="+captionText;
+            new GetURL().execute(emailReqURL);
+            MainActivity.this.setContentView(R.layout.tickview);
+        }
+    }
 }
-
